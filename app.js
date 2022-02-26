f3fd2c55e6d2ec59856d5a339804e61304c3af32
Sobit Prasad <sobitp59@gmail.com>
2022-02-27 00:04:45 +0530

    ex03 done : wiring with a button

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-let user = prompt('What is your name?');
-let userGreet = 'Hi!' + ' ' + user;
-alert(userGreet);
+const btn = document.querySelector('.btn');
+// const ans = function(){
+//     console.log('You clicked! to read more');
+// }
+const  ans = () => {
+console.log('You clicked! to read more');
+
+}
+btn.addEventListener('click', ans);
