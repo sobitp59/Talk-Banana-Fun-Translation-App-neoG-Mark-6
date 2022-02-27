91db422fb088910084c01fce0a30db3245f86ba2
Sobit Prasad <sobitp59@gmail.com>
2022-02-27 09:11:59 +0530

    ex05 done : DOM manipulation

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-// const textArea = document.querySelector('.textArea');
-// const btn = document.querySelector('.btn');
+const textArea = document.querySelector('.textArea');
+const textAreaOutput = document.querySelector('.textOutput');
+const btn = document.querySelector('.btn');
 
-// const translated = () => {
-//     let data = textArea.value;
-//     console.log(data);
-// }
+const translated = () => {
+    let output = document.createElement('div');
+    output.innerText = textArea.value;
+    document.body.appendChild(output);
+    // textAreaOutput.appendChild(output);
+}
 
-// btn.addEventListener('click', translated);
-
-const tagName = document.getElementsByTagName('textarea');
-console.log(tagName);
-
-const btnPrimary =  document.getElementsByClassName("btn-primary");
-console.log(btnPrimary);
-
-const inputBtn = document.getElementById('input-btn');
-console.log(inputBtn);
-
-const tagName2 = document.getElementsByTagName('input');
-console.log(tagName2);
+btn.addEventListener('click', translated);
