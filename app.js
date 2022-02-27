7723a3802152be10e482f7e6362e638a57c68e90
Sobit Prasad <sobitp59@gmail.com>
2022-02-27 08:43:09 +0530

    live exercise done : css selectors

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const textArea = document.querySelector('.textArea');
-const btn = document.querySelector('.btn');
+// const textArea = document.querySelector('.textArea');
+// const btn = document.querySelector('.btn');
 
-const translated = () => {
-    let data = textArea.value;
-    console.log(data);
-}
+// const translated = () => {
+//     let data = textArea.value;
+//     console.log(data);
+// }
 
-btn.addEventListener('click', translated);
+// btn.addEventListener('click', translated);
+
+const tagName = document.getElementsByTagName('textarea');
+console.log(tagName);
+
+const btnPrimary =  document.getElementsByClassName("btn-primary");
+console.log(btnPrimary);
+
+const inputBtn = document.getElementById('input-btn');
+console.log(inputBtn);
+
+const tagName2 = document.getElementsByTagName('input');
+console.log(tagName2);
