550ff0b5ae1557a50da5a1154c528a494ca848fe
Sobit Prasad <sobitp59@gmail.com>
2022-02-28 23:57:38 +0530

    files : styling done

--- app.js
+++ app.js
@@ -4,9 +4,24 @@ const btn = document.querySelector('.btn');
 
 const translated = () => {
     let fromTrans = textArea.value;
-    transatedOutput.innerText = fromTrans;
+    // const url = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=' + fromTrans;
+    // const url = 'https://api.funtranslations.com/translate/minion.json?text=' + fromTrans;
+    const url = 'https://api.funtranslations.com/translate/vulcan.json?text=' + fromTrans;
+    fetch(url)
+    .then(res => res.json())
+    .then(json => {
+    transatedOutput.innerText = json.contents.translated;
+        
+    }) 
 }
 btn.addEventListener('click', translated);
 
 
+// let text1 = 'I am Batman'
+// let text2 = 'I am Spider-man'
+// let text3 = 'I am Shaktiman'
 
+
+// fetch('https://api.funtranslations.com/translate/vulcan.json?text=i am tanay')
+// .then(res => res.json())
+// .then(json => console.log(json))
