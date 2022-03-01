c4690f242f0977d67a37fafbcd17d2eea8f9582e
Sobit Prasad <sobitp59@gmail.com>
2022-03-01 14:28:43 +0530

    files : styling done

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-const textArea = document.querySelector('.textArea');
-const transatedOutput = document.querySelector('.transated-output');
-const btn = document.querySelector('.btn');
+const inputArea = document.querySelector('.input-area');
+const outputArea = document.querySelector('.output-area');
+const btn = document.querySelector('.btn-translate');
+
+
+const url = 'https://api.funtranslations.com/translate/minion.json';
+
+function serverUrl(text){
+    return url + '?text=' + text;
+}
+
+function errorHandler(error){
+    alert(`something wrong with the server! Try again after sometime.`)
+}
 
 const translated = () => {
-    let fromTrans = textArea.value;
-    // const url = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=' + fromTrans;
-    // const url = 'https://api.funtranslations.com/translate/minion.json?text=' + fromTrans;
-    const url = 'https://api.funtranslations.com/translate/vulcan.json?text=' + fromTrans;
-    fetch(url)
+    let engInput = inputArea.value;
+   
+    fetch(serverUrl(engInput))
     .then(res => res.json())
-    .then(json => {
-    transatedOutput.innerText = json.contents.translated;
-        
+    .then(json =>{
+        outputArea.innerText =  json.contents.translated;
     }) 
+    .catch(errorHandler)
+        
 }
 btn.addEventListener('click', translated);
 
 
-// let text1 = 'I am Batman'
-// let text2 = 'I am Spider-man'
-// let text3 = 'I am Shaktiman'
-
-
-// fetch('https://api.funtranslations.com/translate/vulcan.json?text=i am tanay')
-// .then(res => res.json())
-// .then(json => console.log(json))
